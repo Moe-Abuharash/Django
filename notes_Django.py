4ac8e62cd156462dec8c4740e869cafec6b5d3c0
Muhammad Abuharash <muhammad_abuharash2@baylor.edu>
2021-11-15 17:14:47 -0600

    Created classes, started on the project to create the website.

---
+++ notes_Django.py
@@ -0,0 +1,9 @@
+#theres get request which is getting the information from to the browser from the database
+#theres post request sends info to database to update database
+#get request goes from webserver to webpage
+#post request goes from webpage to webserver
+
+
+#to install : pip install -r requirements.txt
+#to freeze: pip install > requirements.txt
+#django-admin startproject learning_log. 
